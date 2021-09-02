cbce3e464329dc879d33b31f9a7be755ebe3b81d
Joan Sebastian Garcia <jsebastian.garcia1995@gmail.com>
2021-09-01 19:10:30 -0500

    update
    added: Created tiles
    added: Movements controller

--- Assets/Scripts/JumpControler.cs
+++ Assets/Scripts/JumpControler.cs
@@ -1,17 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpControler : MonoBehaviour
 {
+    private Rigidbody2D plyr;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        plyr = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // FixedUpdate is called once per frame
+    private void FixedUpdate()
     {
         
     }
--- Assets/Scripts/MovementControler.cs
+++ Assets/Scripts/MovementControler.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementControler : MonoBehaviour
 {
+    private Rigidbody2D plyr;
+
+    public float jumpForce = 9f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        plyr = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // FixedUpdate is called once per frame
+    private void FixedUpdate()
     {
-        
+        // here is the controls for the horizontal movements
+        float dir = Input.GetAxisRaw("Horizontal");
+        plyr.velocity = new Vector2(dir * 6f, 0);
+
+
+        // here the player jumps
+        if (Input.GetButtonDown("Jump"))
+        {
+            plyr.velocity = new Vector2(plyr.velocity.x, jumpForce);
+        }
     }
 }
